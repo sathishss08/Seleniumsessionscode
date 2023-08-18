d969b6d3e232829e1c4c6ba62678e7422c967586
sathishss08 <sathishgd95@hotmail.com>
2023-08-18 14:33:03 +0530

    added payment folder

---
+++ Paymentpage.java
@@ -0,0 +1,21 @@
+package testngsessions;
+
+public class Paymentpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		int i =10;
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
